51eca42595c002729668bdb903cdb0219fa4686a
George Lesica <george@lesica.com>
2021-01-15 09:57:42 -0700

    Add class changes to script

--- topics/02-shell-scripting/control.sh
+++ topics/02-shell-scripting/control.sh
@@ -2,7 +2,23 @@
 
 set -e
 
-if [ "$1" == "hello" ]; then
+greeting=$1
+other=$2
+
+if [ "$greeting" = "hello" ]; then
     echo "hello to you as well"
 fi
+
+names="bob anne pete"
+for i in $names; do
+    echo "there is a file $i"
+done
+
+addsomething() {
+    local value=$1
+    echo $(($value + $other))
+}
+
+addsomething 5
+echo $value
 
