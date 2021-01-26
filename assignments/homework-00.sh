86980714435557a2a1fc5af89226a829c3ce48e9
joeystockdill <46660979+joeystockdill@users.noreply.github.com>
2021-01-25 18:17:15 -0700

    Update homework-00.sh

--- assignments/homework-00.sh
+++ assignments/homework-00.sh
@@ -5,14 +5,17 @@
 # `folder/otherscript.sh` should both be included. Hint: you can do this easily
 # using the `ls` and `grep` commands.
 
-# YOUR SOLUTION
+ls | grep ".sh"
+or it's
+ls | grep -r ".sh"
+
 
 # Count how many ".zip" files there are in the current directory. Your command
 # should output only a numerical value, nothing else. Hint: one way to do this
 # involves two "pipes" (`|`) and the `wc` command. It is also possible, though
 # quite verbose, to do this with a `for` loop.
 
-# YOUR SOLUTION
+ls | grep ".zip" | wc -l
 
 # Imagine that you have a bunch of source code files with ".py" file extensions.
 # We want to find all the lines in these files that define new functions (they
@@ -20,5 +23,19 @@
 # the filename and line number. There are a bunch of ways to do this, some
 # simpler than others.
 
-# YOUR SOLUTION
+files="ls | egrep '.py|def' *.py"
+for entry in $files
+do
+    echo $entry
+    grep -hn $entry
+done
 
+
+or would the following work since its all python files?
+
+files="ls | egrep 'def' *.py"
+for entry in $files
+do
+    echo $entry
+    grep -hn $entry
+done
